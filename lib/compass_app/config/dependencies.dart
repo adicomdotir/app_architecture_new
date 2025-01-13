7e8cea9603bbb68777fd8c0ce3996403843e34b5
adicomdotir <adicom.ir@gmail.com>
2025-01-13 13:09:37 +0330

    feat: compass_app part 2

--- lib/compass_app/config/dependencies.dart
+++ lib/compass_app/config/dependencies.dart
@@ -1,45 +1,45 @@
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
+import '../data/repositories/activity/activity_repository.dart';
+import '../data/repositories/activity/activity_repository_local.dart';
 import '../data/repositories/auth/auth_repository.dart';
 import '../data/repositories/auth/auth_repository_dev.dart';
 import '../data/repositories/booking/booking_repository.dart';
 import '../data/repositories/booking/booking_repository_local.dart';
+// import '../data/repositories/booking/booking_repository_remote.dart';
+import '../data/repositories/continent/continent_repository.dart';
+import '../data/repositories/continent/continent_repository_local.dart';
+// import '../data/repositories/continent/continent_repository_remote.dart';
+import '../data/repositories/destination/destination_repository.dart';
+import '../data/repositories/destination/destination_repository_local.dart';
+// import '../data/repositories/destination/destination_repository_remote.dart';
+import '../data/repositories/itinerary_config/itinerary_config_repository.dart';
+import '../data/repositories/itinerary_config/itinerary_config_repository_memory.dart';
 import '../data/repositories/user/user_repository.dart';
 import '../data/repositories/user/user_repository_local.dart';
-// import '../data/repositories/booking/booking_repository_remote.dart';
-// import '../data/repositories/continent/continent_repository.dart';
-// import '../data/repositories/continent/continent_repository_local.dart';
-// import '../data/repositories/continent/continent_repository_remote.dart';
-// import '../data/repositories/destination/destination_repository.dart';
-// import '../data/repositories/destination/destination_repository_local.dart';
-// import '../data/repositories/destination/destination_repository_remote.dart';
-// import '../data/repositories/itinerary_config/itinerary_config_repository.dart';
-// import '../data/repositories/itinerary_config/itinerary_config_repository_memory.dart';
-// import '../data/repositories/user/user_repository.dart';
-// import '../data/repositories/user/user_repository_local.dart';
 // import '../data/repositories/user/user_repository_remote.dart';
 // import '../data/services/api/api_client.dart';
 // import '../data/services/api/auth_api_client.dart';
 import '../data/services/local/local_data_service.dart';
 // import '../data/services/shared_preferences_service.dart';
-// import '../domain/use_cases/booking/booking_create_use_case.dart';
-// import '../domain/use_cases/booking/booking_share_use_case.dart';
+import '../domain/use_cases/booking/booking_create_use_case.dart';
+import '../domain/use_cases/booking/booking_share_use_case.dart';
 
 /// Shared providers for all configurations.
 List<SingleChildWidget> _sharedProviders = [
-  // Provider(
-  //   lazy: true,
-  //   create: (context) => BookingCreateUseCase(
-  //     destinationRepository: context.read(),
-  //     activityRepository: context.read(),
-  //     bookingRepository: context.read(),
-  //   ),
-  // ),
-  // Provider(
-  //   lazy: true,
-  //   create: (context) => BookingShareUseCase.withSharePlus(),
-  // ),
+  Provider(
+    lazy: true,
+    create: (context) => BookingCreateUseCase(
+      destinationRepository: context.read(),
+      activityRepository: context.read(),
+      bookingRepository: context.read(),
+    ),
+  ),
+  Provider(
+    lazy: true,
+    create: (context) => BookingShareUseCase.withSharePlus(),
+  ),
 ];
 
 /// Configure dependencies for remote data.
@@ -105,29 +105,29 @@ List<SingleChildWidget> get providersLocal {
     Provider.value(
       value: LocalDataService(),
     ),
-    // Provider(
-    //   create: (context) => DestinationRepositoryLocal(
-    //     localDataService: context.read(),
-    //   ) as DestinationRepository,
-    // ),
-    // Provider(
-    //   create: (context) => ContinentRepositoryLocal(
-    //     localDataService: context.read(),
-    //   ) as ContinentRepository,
-    // ),
-    // Provider(
-    //   create: (context) => ActivityRepositoryLocal(
-    //     localDataService: context.read(),
-    //   ) as ActivityRepository,
-    // ),
+    Provider(
+      create: (context) => DestinationRepositoryLocal(
+        localDataService: context.read(),
+      ) as DestinationRepository,
+    ),
+    Provider(
+      create: (context) => ContinentRepositoryLocal(
+        localDataService: context.read(),
+      ) as ContinentRepository,
+    ),
+    Provider(
+      create: (context) => ActivityRepositoryLocal(
+        localDataService: context.read(),
+      ) as ActivityRepository,
+    ),
     Provider(
       create: (context) => BookingRepositoryLocal(
         localDataService: context.read(),
       ) as BookingRepository,
     ),
-    // Provider.value(
-    //   value: ItineraryConfigRepositoryMemory() as ItineraryConfigRepository,
-    // ),
+    Provider.value(
+      value: ItineraryConfigRepositoryMemory() as ItineraryConfigRepository,
+    ),
     Provider(
       create: (context) => UserRepositoryLocal(
         localDataService: context.read(),
--- lib/compass_app/data/services/local/local_data_service.dart
+++ lib/compass_app/data/services/local/local_data_service.dart
@@ -8,43 +8,43 @@ import 'package:flutter/services.dart';
 
 import '../../../config/assets.dart';
 import '../../../domain/models/activity/activity.dart';
-// import '../../../domain/models/continent/continent.dart';
+import '../../../domain/models/continent/continent.dart';
 import '../../../domain/models/destination/destination.dart';
 import '../../../domain/models/user/user.dart';
 
 class LocalDataService {
-  // List<Continent> getContinents() {
-  //   return [
-  //     const Continent(
-  //       name: 'Europe',
-  //       imageUrl: 'https://rstr.in/google/tripedia/TmR12QdlVTT',
-  //     ),
-  //     const Continent(
-  //       name: 'Asia',
-  //       imageUrl: 'https://rstr.in/google/tripedia/VJ8BXlQg8O1',
-  //     ),
-  //     const Continent(
-  //       name: 'South America',
-  //       imageUrl: 'https://rstr.in/google/tripedia/flm_-o1aI8e',
-  //     ),
-  //     const Continent(
-  //       name: 'Africa',
-  //       imageUrl: 'https://rstr.in/google/tripedia/-nzi8yFOBpF',
-  //     ),
-  //     const Continent(
-  //       name: 'North America',
-  //       imageUrl: 'https://rstr.in/google/tripedia/jlbgFDrSUVE',
-  //     ),
-  //     const Continent(
-  //       name: 'Oceania',
-  //       imageUrl: 'https://rstr.in/google/tripedia/vxyrDE-fZVL',
-  //     ),
-  //     const Continent(
-  //       name: 'Australia',
-  //       imageUrl: 'https://rstr.in/google/tripedia/z6vy6HeRyvZ',
-  //     ),
-  //   ];
-  // }
+  List<Continent> getContinents() {
+    return [
+      const Continent(
+        name: 'Europe',
+        imageUrl: 'https://rstr.in/google/tripedia/TmR12QdlVTT',
+      ),
+      const Continent(
+        name: 'Asia',
+        imageUrl: 'https://rstr.in/google/tripedia/VJ8BXlQg8O1',
+      ),
+      const Continent(
+        name: 'South America',
+        imageUrl: 'https://rstr.in/google/tripedia/flm_-o1aI8e',
+      ),
+      const Continent(
+        name: 'Africa',
+        imageUrl: 'https://rstr.in/google/tripedia/-nzi8yFOBpF',
+      ),
+      const Continent(
+        name: 'North America',
+        imageUrl: 'https://rstr.in/google/tripedia/jlbgFDrSUVE',
+      ),
+      const Continent(
+        name: 'Oceania',
+        imageUrl: 'https://rstr.in/google/tripedia/vxyrDE-fZVL',
+      ),
+      const Continent(
+        name: 'Australia',
+        imageUrl: 'https://rstr.in/google/tripedia/z6vy6HeRyvZ',
+      ),
+    ];
+  }
 
   Future<List<Activity>> getActivities() async {
     final json = await _loadStringAsset(Assets.activities);
